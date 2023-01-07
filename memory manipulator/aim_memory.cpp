5349b71997af74ec6805b9884c17ca3f390810c3
Youngsik Yang <vacu9708@gmail.com>
2023-01-08 07:20:52 +0900

    a

--- memory manipulator/aim_memory.cpp
+++ memory manipulator/aim_memory.cpp
@@ -81,8 +81,6 @@ void get_process_memory(DWORD start, DWORD end) {
 		}
 		if (!(mem_info.State & MEM_COMMIT) || !(mem_info.Type & MEM_PRIVATE) || mem_info.Type & MEM_IMAGE ||
 			mem_info.Protect & PAGE_GUARD || !(mem_info.Protect & PAGE_READWRITE) || mem_info.Protect & PAGE_NOACCESS) continue;
-		//if ((BYTE*)mem_info.BaseAddress - (BYTE*)0x700000000000 > 0) continue; // Skip module
-		//if ((BYTE*)mem_info.BaseAddress - (BYTE*)0x10000000000 < 0) continue; // Skip stack
 		process_ptrs->push_back(Mem_info((BYTE*)mem_info.BaseAddress, (DDWORD)mem_info.RegionSize));
 		if (end && (i >= end))
 			break;
@@ -104,42 +102,61 @@ vector<string> parse(string str) {
 	return choice;
 }
 
-long long ptr2val(BYTE* ptr, BYTE type) {
+long long addr2val(BYTE* addr, float type) {
 	if (type == 1)
-		return *(unsigned char*)ptr;
-	else if (type == -1)
-		return *(char*)ptr;
+		return *(char*)addr;
 	else if (type == 2)
-		return *(unsigned short*)ptr;
-	else if (type == -2)
-		return *(short*)ptr;
+		return *(short*)addr;
 	else if (type == 4)
-		return *(float*)ptr;
+		return *(long*)addr;
 	else if (type == 8)
-		return *(double*)ptr;
+		return *(long long*)addr;
+	else if (type == 4.1)
+		return *(float*)addr;
+	else if (type == 8.1)
+		return *(double*)addr;
 }
 
-BYTE decide_type(string choice) {
-	BYTE type = stoull(choice);
-	bytes_to_read = abs(type);
+DDWORD addr2uval(BYTE* addr, float type) {
+	if (type == -1)
+		return *(unsigned char*)addr;
+	else if (type == -2)
+		return *(unsigned short*)addr;
+	else if (type == -4)
+		return *(unsigned long*)addr;
+	else if (type == -8)
+		return *(DDWORD*)addr;
+}
+
+float decide_type(string choice) {
+	float type = stof(choice);
+	bytes_to_read = (BYTE)abs(type);
 	delete[] read_buffer;
 	read_buffer = new BYTE[bytes_to_read];
 	return type;
 }
 
+bool compare_str(BYTE* chars, string str) {
+	for (short i = 0; i < bytes_to_read; i++) {
+		if ((BYTE)chars[i] != (BYTE)str[i])
+			return false;
+	}
+	return true;
+}
+
 void detect(BYTE type, bool is_even) {
 	get_process_memory(0, 0);
 	DDWORD count = 0;
-	DDWORD target = 0, limit = 0;
+	long long target = 0, limit = 0;
+	string target_str;
+
 	if (!filtered) {
 		short** prev_values = NULL;
-		printf("1 [target] / 2: unknown / 0: exit\n");
+		printf("1: unknown / 2 [target] / 3 [target str] / 0: exit\n");
 		string input; getline(cin, input, '\n');
 		vector<string> choice = parse(input);
 		vector<string> choice2;
-		if (choice[0] == "1")
-			target = stoull(choice[1]);
-		else if (choice[0] == "2") {
+		if (choice[0] == "1") { // unknown
 			// Make prev_values
 			prev_values = new short* [process_ptrs->size()];
 			for (DWORD i = 0; i < process_ptrs->size(); i++) {
@@ -151,11 +168,12 @@ void detect(BYTE type, bool is_even) {
 				}
 				prev_values[i] = new short[block_size / 2 + 1];
 				for (DDWORD j = 0; j < block_size - bytes_to_read + 1; j++) {
+					// Skip even or odd address
 					BYTE remainder = (DDWORD)(process_ptrs->at(i).addr + j) % 2;
 					if ((is_even && remainder != 0) || (!is_even && remainder != 1))
 						continue;
 
-					prev_values[i][j / 2] = ptr2val(block_buffer + j, type);
+					prev_values[i][j / 2] = addr2val(block_buffer + j, type);
 					count++;
 				}
 				delete[] block_buffer;
@@ -164,10 +182,10 @@ void detect(BYTE type, bool is_even) {
 			count = 0;
 
 			// Choose a way to compare with prev_values
-			printf("2: increased [limit] / 3: decreased [limit] / 0: exit\n");
+			printf("1: increased [limit] / 2: decreased [limit]\n");
 			string input; getline(cin, input, '\n');
 			choice2 = parse(input);
-			if (choice2[0] == "2" || choice2[0] == "3")
+			if (choice2[0] == "1" || choice2[0] == "2")
 				limit = stoull(choice2[1]);
 			else {
 				for (DWORD i = 0; i < process_ptrs->size(); i++)
@@ -175,6 +193,15 @@ void detect(BYTE type, bool is_even) {
 				delete[] prev_values;
 				return;
 			}
+		}
+		else if (choice[0] == "2") {// target
+			target = stoll(choice[1]);
+		}
+		else if (choice[0] == "3") { // target string
+			target_str = choice[1];
+			bytes_to_read = target_str.length();
+			delete[] read_buffer;
+			read_buffer = new BYTE[bytes_to_read];
 		}
 		else return;
 
@@ -189,60 +216,70 @@ void detect(BYTE type, bool is_even) {
 			}
 			for (DDWORD j = 0; j < block_size - bytes_to_read + 1; j++) {
 				BYTE* addr = process_ptrs->at(i).addr + j;
+				// Skip even or odd address
 				BYTE remainder = (DDWORD)addr % 2;
 				if ((is_even && remainder != 0) || (!is_even && remainder != 1))
 					continue;
 
-				long long value = ptr2val(block_buffer + j, type);
-
-				if (choice[0] == "1" && value == target) {
-					filtered->push_back(Point(addr, value));
-					count++;
-					//printf("%p %d\n", addr, data);
+				if (choice[0] == "3") { // target string
+					for (short byte = 0; byte < bytes_to_read; byte++)
+						read_buffer[byte] = block_buffer[j + byte];
+					if (compare_str(read_buffer, target_str)) {
+						filtered->push_back(Point(addr, NULL));
+						count++;
+						continue;
+					}
 				}
-				else if (choice[0] == "2") {
+
+				long long value = addr2val(block_buffer + j, type);
+				if (choice[0] == "1") { // unknown
 					short prev_value = prev_values[i][j / 2];
-					if ((choice2[0] == "2" && value > prev_value && (value - prev_value) <= limit) ||
-						(choice2[0] == "3" && value < prev_value && (prev_value - value) <= limit)) {
+					if ((choice2[0] == "1" && value > prev_value && (value - prev_value) <= limit) ||
+						(choice2[0] == "2" && value < prev_value && (prev_value - value) <= limit)) {
 							filtered->push_back(Point(addr, value));
 							count++;
 					}
 				}
+				else if (choice[0] == "2" && value == target) { // target
+						filtered->push_back(Point(addr, value));
+						count++;
+				}
 			}
 			delete[] block_buffer;
-			if (choice[0] == "2")
+			if (choice[0] == "1") // unknown
 				delete[] prev_values[i];
 		}
-		if (choice[0] == "2") {
+		if (choice[0] == "1") // unknown
 			delete[] prev_values;
-		}
 		printf("Found results: %lld\n", count);
 		count = 0;
 	}
 
 	// Keep filtering
 	while (1) {
-		printf("1 [target] / 2: increased [limit] / 3: decreased [limit] 4: same / 0: exit\n");
+		printf("1: increased [limit] / 2: decreased [limit] / 3: same / 4 [target] / 5: same string\n");
 		string input; getline(cin, input, '\n');
 		vector<string> choice = parse(input);
-		if (choice[0] == "1")
-			target = stoul(choice[1]);
-		else if (choice[0] == "2" || choice[0] == "3")
-			limit = stoull(choice[1]);
-		else if(choice[0] == "0") return;
+		if (choice[0] == "1" || choice[0] == "2")
+			limit = stoll(choice[1]);
+		else if (choice[0] == "3");
+		else if (choice[0] == "4")
+			target = stoll(choice[1]);
+		else if (choice[0] == "5");
+		else return;
 
 		vector<Point>* new_filtered = new vector<Point>();
 		for (Point point : *filtered) {
 			if (!ReadProcessMemory(process, point.addr, read_buffer, bytes_to_read, NULL)) continue;
-			long long value = ptr2val(read_buffer, type);
+			long long value = addr2val(read_buffer, type);
 
-			if ((choice[0] == "1" && value == target) ||
-				(choice[0] == "2" && value > point.value && (value - point.value) <= limit) ||
-				(choice[0] == "3" && value < point.value && (point.value - value) <= limit) ||
-				(choice[0] == "4" && value == point.value)) {
+			if ((choice[0] == "1" && value > point.value && (value - point.value) <= limit) || // increased
+				(choice[0] == "2" && value < point.value && (point.value - value) <= limit) || // decreased
+				(choice[0] == "3" && value == point.value) || // same
+				(choice[0] == "4" && value == target) || // target
+				(choice[0] == "5" && compare_str(read_buffer, target_str))) { // target str
 				new_filtered->push_back(Point(point.addr, value));
 				count++;
-				//printf("%p %d\n", point.addr, data);
 			}
 		}
 		delete filtered;
@@ -258,10 +295,18 @@ void get_result(int type) {
 	for (Point point : *filtered) {
 		if(!ReadProcessMemory(process, point.addr, read_buffer, bytes_to_read, NULL))
 			continue;
-		long long value = ptr2val(read_buffer, type);
-		if (value == 0) continue;
-		fprintf(fp, "(%d)addr: %lld val: %lld\n", i, point.addr, value);
-		printf("(%d)addr: %lld val: %lld\n", i++, point.addr, value);
+		long long value;
+		DDWORD u_value;
+		if (type > 0) {
+			value = addr2val(read_buffer, type);
+			fprintf(fp, "(%d)addr: %lld val: %lld\n", i, point.addr, value);
+			printf("(%d)addr: %lld val: %lld\n", i++, point.addr, value);
+		}
+		else {
+			u_value = addr2uval(read_buffer, type);
+			fprintf(fp, "(%d)addr: %lld val: %lld\n", i, point.addr, u_value);
+			printf("(%d)addr: %lld val: %lld\n", i++, point.addr, u_value);
+		}
 	}
 	fclose(fp);
 }
@@ -271,10 +316,19 @@ void read_range(DDWORD addr, short range, char type) {
 	for (short offset = -range; offset <= range; offset++) {
 		if (!ReadProcessMemory(process, (BYTE*)addr + offset, read_buffer, bytes_to_read, NULL))
 			continue;
-		long long value = ptr2val(read_buffer, type);
-		if (value == 0) continue;
-		fprintf(fp, "(%d)addr: %lld val: %lld\n", offset, addr + offset, value);
-		printf("(%d)addr: %lld val: %lld\n", offset, addr + offset, value);
+
+		long long value;
+		DDWORD u_value;
+		if (type > 0) {
+			value = addr2val(read_buffer, type);
+			fprintf(fp, "(%d)addr: %lld val: %lld\n", offset, addr + offset, value);
+			printf("(%d)addr: %lld val: %lld\n", offset, addr + offset, value);
+		}
+		else {
+			u_value = addr2uval(read_buffer, type);
+			fprintf(fp, "(%d)addr: %lld val: %lld\n", offset, addr + offset, u_value);
+			printf("(%d)addr: %lld val: %lld\n", offset, addr + offset, u_value);
+		}
 	}
 	fclose(fp);
 }
@@ -284,7 +338,7 @@ void read_ptr(DDWORD ptr_addr, char type) {
 	BYTE ptr[size];
 	if(!ReadProcessMemory(process, (BYTE*)ptr_addr, ptr, 8, NULL)) return;
 	if(!ReadProcessMemory(process, (BYTE*)*(DDWORD*)ptr, read_buffer, bytes_to_read, NULL)) return;
-	printf("Result: %lld\n", ptr2val(read_buffer, type));
+	printf("Result: %lld\n", addr2val(read_buffer, type));
 }
 
 void write(DDWORD addr, string val, char type) {
@@ -302,6 +356,30 @@ void write(DDWORD addr, string val, char type) {
 		long long buffer = stoll(val);
 		if (!WriteProcessMemory(process, (BYTE*)addr, &buffer, bytes_to_read, NULL))
 			printf("Error\n");
+	}
+}
+
+void change_through(string val, char type) {
+	for (Point point : *filtered) {
+		printf("%lld changed. 1: next / 0: exit", point.addr);
+		string input; getline(cin, input, '\n');
+		if (input == "0")
+			return;
+		if (type == 4) {
+			float buffer = stof(val);
+			if (!WriteProcessMemory(process, (BYTE*)point.addr, &buffer, bytes_to_read, NULL))
+				printf("Error\n");
+		}
+		else if (type == 8) {
+			double buffer = stod(val);
+			if (!WriteProcessMemory(process, (BYTE*)point.addr, &buffer, bytes_to_read, NULL))
+				printf("Error\n");
+		}
+		else {
+			long long buffer = stoll(val);
+			if (!WriteProcessMemory(process, (BYTE*)point.addr, &buffer, bytes_to_read, NULL))
+				printf("Error\n");
+		}
 	}
 }
 
@@ -343,13 +421,13 @@ void main() {
 	printf("Process %d opened\n", pID);
 	ListProcessModules();
 
-	BYTE type = 1;
+	float type = 1;
 	read_buffer = new BYTE[1];
-	freeze_value();
+	//freeze_value();
 	while (true) {
 		printf("1 [type]: set type / 2: detect even / 3: detect odd\n");
-		printf("4: get result / 5 [ptr_addr]: read ptr / 6 [addr] [range]: read range\n");
-		printf("7 [addr] [val]: write int / 8 [addr] [val]: write float / 9 [addr] [val]: write double 0 : exit\n");
+		printf("4: get result / 5 [addr] [range]: read range / 6 [ptr_addr]: read ptr / \n");
+		printf("7 [addr] [val]: write int / 8 [val]: change through\n");
 		string input; getline(cin, input, '\n');
 		vector<string> choice = parse(input);
 		if (choice[0] == "1")
@@ -361,15 +439,13 @@ void main() {
 		else if (choice[0] == "4")
 			get_result(type);
 		else if (choice[0] == "5")
-			read_ptr(stoull(choice[1]), type);
-		else if (choice[0] == "6")
 			read_range(stoull(choice[1]), stoi(choice[2]), type);
+		else if (choice[0] == "6")
+			read_ptr(stoull(choice[1]), type);
 		else if (choice[0] == "7")
 			write(stoull(choice[1]), choice[2], type);
 		else if (choice[0] == "8")
-			write(stoull(choice[1]), choice[2], type);
-		else if (choice[0] == "9")
-			write(stoull(choice[1]), choice[2], type);
+			change_through(choice[1], type);
 		else return;
 	}
 }
